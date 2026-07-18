91b1dce96d18fdea7ee6ad9de00ad9bc8137bb6e
praveen <praveenramesh5267@gmail.com>
2026-07-18 16:49:14 +0530

    change update

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,107 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface HomeAwards extends Struct.ComponentSchema {
+  collectionName: 'components_home_awards';
+  info: {
+    displayName: 'awards';
+  };
+  attributes: {
+    logos: Schema.Attribute.Component<'shared.imagegroup', true>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface HomeExpertise extends Struct.ComponentSchema {
+  collectionName: 'components_home_expertise';
+  info: {
+    displayName: 'expertise';
+  };
+  attributes: {
+    bgimage: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    icon: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface HomeHero extends Struct.ComponentSchema {
+  collectionName: 'components_home_heroes';
+  info: {
+    displayName: 'hero';
+  };
+  attributes: {
+    buttontext: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+    isButton: Schema.Attribute.Boolean;
+    subtitle: Schema.Attribute.Text;
+    title: Schema.Attribute.Text;
+  };
+}
+
+export interface HomeSolarandstorage extends Struct.ComponentSchema {
+  collectionName: 'components_home_solarandstorages';
+  info: {
+    displayName: 'solarandstorage';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    number: Schema.Attribute.Integer;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface HomeWhychooseus extends Struct.ComponentSchema {
+  collectionName: 'components_home_whychooseuses';
+  info: {
+    displayName: 'whychooseus';
+  };
+  attributes: {
+    data: Schema.Attribute.Component<'shared.cardgroup', true>;
+    subtitle: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedCardgroup extends Struct.ComponentSchema {
+  collectionName: 'components_shared_cardgroups';
+  info: {
+    displayName: 'cardgroup';
+  };
+  attributes: {
+    bgimage: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    description: Schema.Attribute.String;
+    icon: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    suffix: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+    value: Schema.Attribute.Integer;
+  };
+}
+
+export interface SharedImagegroup extends Struct.ComponentSchema {
+  collectionName: 'components_shared_imagegroups';
+  info: {
+    displayName: 'imagegroup';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export namespace Public {
+    export interface ComponentSchemas {
+      'home.awards': HomeAwards;
+      'home.expertise': HomeExpertise;
+      'home.hero': HomeHero;
+      'home.solarandstorage': HomeSolarandstorage;
+      'home.whychooseus': HomeWhychooseus;
+      'shared.cardgroup': SharedCardgroup;
+      'shared.imagegroup': SharedImagegroup;
+    }
+  }
+}
