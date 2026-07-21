e8acae94a922181c73e2fa90fb92d19380c9cd66
thrun-11 <91716287+thrun-11@users.noreply.github.com>
2026-07-21 12:59:50 +0530

    seed code addedback

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import type { Core } from '@strapi/strapi';
+import { runSeed } from './seed';
 
 export default {
   register({ strapi }: { strapi: Core.Strapi }) {},
-  bootstrap({ strapi }: { strapi: Core.Strapi }) {},
+  async bootstrap({ strapi }: { strapi: Core.Strapi }) {
+    await runSeed(strapi);
+  },
 };
