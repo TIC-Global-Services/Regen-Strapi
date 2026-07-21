48e624334f2f4bff70d65a03398022fa6571b762
thrun-11 <91716287+thrun-11@users.noreply.github.com>
2026-07-21 11:57:00 +0530

    seed script removed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import type { Core } from '@strapi/strapi';
-import { runSeed } from './seed';
 
 export default {
   register({ strapi }: { strapi: Core.Strapi }) {},
-  async bootstrap({ strapi }: { strapi: Core.Strapi }) {
-    await runSeed(strapi);
-  },
+  bootstrap({ strapi }: { strapi: Core.Strapi }) {},
 };
