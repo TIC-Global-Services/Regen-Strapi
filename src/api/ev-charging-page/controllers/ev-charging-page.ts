f6cb540a7fbf5b2dc0441c7dd601cc77f18a7a44
thrun-11 <91716287+thrun-11@users.noreply.github.com>
2026-08-08 07:28:51 +0530

    ev-charging controllers,routes,services

---
+++ src/api/ev-charging-page/controllers/ev-charging-page.ts
@@ -0,0 +1,7 @@
+/**
+ * ev-charging-page controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::ev-charging-page.ev-charging-page');
---
+++ src/api/ev-charging-page/routes/ev-charging-page.ts
@@ -0,0 +1,7 @@
+/**
+ * ev-charging-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::ev-charging-page.ev-charging-page');
