afdd600ead6fdb03e60ed26103469a241666c05b
thrun-11 <91716287+thrun-11@users.noreply.github.com>
2026-08-28 13:27:34 +0530

    api files update

--- src/api/promotion-page/services/promotion-page.ts
+++ src/api/promotion-page/services/promotion-page.ts
@@ -4,4 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::promotion-page.promotion-page' as any);
+export default factories.createCoreService('api::promotion-page.promotion-page');
