7656086caa0c5e67d4f8d3eafb7cdf7c912ad807
thrun-11 <91716287+thrun-11@users.noreply.github.com>
2026-08-31 12:10:53 +0530

    aboutpage data added

--- src/seed/data/about-page.ts
+++ src/seed/data/about-page.ts
@@ -14,25 +14,255 @@ export const aboutSections: Section[] = [
     __component: "about.awards",
     cards: [
       {
-        title: "Finalist - Smart Installer, Smart Energy Excellence Awards 2026",
+        title: "Finalist - Smart Installer, Smart Energy Excellence Awards 2026.",
         description: "Regen Power is proud to be recognised nationally. Finalist – Smart Installer, Smart Energy Excellence Awards 2026.",
         badge: null,
         badgeSizeClass: "h-48 w-48",
       },
       {
-        title: "#1 Most Popular Solar Installer in Western Australia and\n#2 Most Popular in Australia by SunWiz Awards 2026",
-        description: "Regen Power is proud to be recognised nationally. Finalist – Smart Installer, Smart Energy Excellence Awards 2026.",
+        title: "#1 Most Popular Solar Installer in Western Australia and #2 Most Popular in Australia by SunWiz Awards 2026",
+        description: "RegenPower has been recognised as the #1 Most Popular Solar Installer in Western Australia and #2 Most Popular in Australia by SunWiz Awards 2026. This recognition reflects our commitment to delivering quality solar solutions and exceptional customer experiences across the nation.",
         badge: null,
         badgeSizeClass: "h-64 w-72",
       },
       {
-        title: "Finalist - Smart Installer, Smart Energy Excellence Awards 2026",
-        description: "Regen Power is proud to be recognised nationally. Finalist – Smart Installer, Smart Energy Excellence Awards 2026.",
+        title: "ProductReview.com.au Awards Winner 2026",
+        description: "Regen Power has won the ProductReview.com.au Award in the Solar Installer Category for the sixth year in a row year (2021, 2022, 2023, 2024, 2025 & 2026). The award is given to the solar company that earned the highest 5-star rating in the calendar year 2025.",
         badge: null,
       },
       {
-        title: "ProductReview.com.au Awards Winner 2026",
-        description: "Regen Power has won the ProductReview.com.au Award in the Solar Installer Category for the sixth year in a row year (2021, 2022, 2023, 2024, 2025 & 2026). The award is given to the solar company that earned the highest 5-star rating in the calendar year 2025.",
+        title: "Regen Power Named Western Australia’s Best-Rated Solar Installer 2025 by SolarQuotes",
+        description: "Regen Power is proud to announce its latest recognition as Western Australia’s Best-Rated Solar Installer for 2025, awarded by SolarQuotes, one of Australia’s most respected and independent renewable energy review platforms.",
+        badge: null,
+      },
+      {
+        title: "Top 5 Best Rated Solar companies",
+        description: "Regen Power is in the Top 5 Best Rated Solar companies in Australia for quality of excellent review.",
+        badge: null,
+      },
+      {
+        title: "Hall of Fame",
+        description: "31st Belmont & Western Australian Small Business Awards 2024.",
+        badge: null,
+      },
+      {
+        title: "Celebrating 22nd Anniversary",
+        description: "Regen Power is celebrating completing 22 years in the industry.",
+        badge: null,
+      },
+      {
+        title: "Completed 45,000 installations",
+        description: "Regen Power has completed 45,000 residential rooftop solar installations.",
+        badge: null,
+      },
+      {
+        title: "ProductReview.com.au Awards Winner 2025",
+        description: "Regen Power has won the ProductReview.com.au Award in the Solar Installer Category for the fifth year in a row year (2021, 2022, 2023, 2024 & 2025). The award is given to the solar company that earned the highest 5-star rating in the calendar year 2024.",
+        badge: null,
+      },
+      {
+        title: "The CEO Magazine’s 2024 Executive of the Year Awards Finalist",
+        description: "Dr. Nikhil Jayaraj, Managing Director of Regen Power, was honoured as a finalist in The CEO Magazine’s Executive of the Year Awards 2024 for his exceptional leadership and transformative impact in the renewable energy sector. This recognition celebrates his achievements in driving innovation, sustainable growth, and academic contributions to the industry.",
+        badge: null,
+      },
+      {
+        title: "EUPD INSTALLER AWARD AUSTRALIA 2024",
+        description: "Regen Power, an award-winning leader in renewable energy, proudly receives the EUPD Research “Top Brand PV Installer 2024” award for the second consecutive year.",
+        badge: null,
+      },
+      {
+        title: "40 UNDER 40 WA Awards 2024",
+        description: "Nikhil Jayaraj, Managing Director, Regen Power Pty Ltd was second time named as a winner of Business News 40under40 Awards 2024.",
+        badge: null,
+      },
+      {
+        title: "SUNWIZ Top 5 All Star Award Winner 2023",
+        description: "Achieved the highest customer ratings across various platforms, to be among the top 5 in Australia — Sunwiz All Star Award",
+        badge: null,
+      },
+      {
+        title: "SUNWIZ No1 Combined WA Award Winner 2023",
+        description: "Recognized as the NO 1 solar provider for the most number of solar installations in WA",
+        badge: null,
+      },
+      {
+        title: "ProductReview Awards Winner 2024",
+        description: "Regen Power has won the ProductReview.com.au Award in the Solar Installer Category for the fourth year in a row year (2021, 2022, 2023 & 2024). The award is given to the solar company that earned the highest 5-star rating in the calendar year 2023.",
+        badge: null,
+      },
+      {
+        title: "EUPD Research Installer Award 2023",
+        description: "Recognition for unwavering commitment to quality and excellence.",
+        badge: null,
+      },
+      {
+        title: "Telstra Business Awards 2024 Finalist",
+        description: "WA state finalist under “promoting sustainability” category",
+        badge: null,
+      },
+      {
+        title: "The CEO Magazine Awards 2023 finalist",
+        description: "Mr. Nikhil Jayaraj was selected as a finalist for Managing Director of the Year in The CEO Magazine’s 2023 Executive of the Year Awards.",
+        badge: null,
+      },
+      {
+        title: "Stevie International Business Awards 2023",
+        description: "Regen Power and its Managing Director Mr. Nikhil Jayaraj was named the winner of a Gold Stevie® Award in the “Fastest-Growing Company of the Year – in Asia, Australia and New Zealand” and “Best Entrepreneur – Energy” category in The 20th Annual International Business Awards®.",
+        badge: null,
+      },
+      {
+        title: "Completed 40,000 installations",
+        description: "Regen Power has completed 40,000 residential rooftop solar installations.",
+        badge: null,
+      },
+      {
+        title: "FT High-Growth Companies 2023",
+        description: "Regen Power ranked 387th in the List of High-Growth Companies of Asia Pacific by FT for the year of 2023.",
+        badge: null,
+      },
+      {
+        title: "ProductReview Awards Winner 2023",
+        description: "Regen Power has won the ProductReview.com.au Award in the Solar Installer Category for the third year in a row year (2021, 2022 & 2023). The award is given to the solar company that earned the highest 5-star rating in the calendar year 2022",
+        badge: null,
+      },
+      {
+        title: "Belmont & WA Business Awards 2022 – \"Enterprise of the Year\" category",
+        description: "Regen Power has been named the winner of the 29th Belmont & Western Australian Award 2022 in the category of “Enterprise of the Year”. The award recognises an organisation’s overall business excellence.",
+        badge: null,
+      },
+      {
+        title: "Completed 35,000 installations",
+        description: "Regen Power has completed 35,000 residential rooftop solar installations.",
+        badge: null,
+      },
+      {
+        title: "FT High-Growth Companies Asia-Pacific",
+        description: "Regen Power ranked 424th in the List of High-Growth Companies of Asia Pacific by FT for the year of 2022.",
+        badge: null,
+      },
+      {
+        title: "2022 ProductReview.com.au Award",
+        description: "Regen Power has won the ProductReview Award in the Solar Installer Category for the second year in a row year (2021 & 2022). The award is given to the solar company that earned the highest 5-star rating in the calendar year 2021",
+        badge: null,
+      },
+      {
+        title: "2021 Belmont and Western Australia Business Awards",
+        description: "Regen Power has been named the winner of the 28th Belmont & Western Australian Award 2021 in the category of ‘Sustainability.’ The award recognises an organisation that is conscious of the environment, sustainability, energy efficiency, and circular economy practices.",
+        badge: null,
+      },
+      {
+        title: "Western Australian of the year Award",
+        description: "Emeritus Professor Chem Nayar, CEO Regen Power, is the finalist of Western Australian of the Year Awards 2021 in the Professions Award category",
+        badge: null,
+      },
+      {
+        title: "Financial Times High Growth Company in Asia-Pacific 2021",
+        description: "Regen Power ranked as 326th High Growth Company in Asia-Pacific 2021 by Financial Times",
+        badge: null,
+      },
+      {
+        title: "Brett Lee, Brand Ambassador 2021",
+        description: "Australian Cricket Legend and top bowler in the world joins Regen Power as brand ambassador",
+        badge: null,
+      },
+      {
+        title: "ProductReview.com.au 2021 Award Winner",
+        description: "ProductReview.com.au 2021 Award winner Regen Power is proud to be 2021 ProductReview Award Winner. Regen Power has 4.8 Stars rating by the ProductReview.com.au Community. ProductReview.com.au is Australia’s #1 consumer review website. Since 2003, ProductReview has over 90,000 of Australia’s top products & services",
+        badge: null,
+      },
+      {
+        title: "2021 WA Australian of the Year – Finalist",
+        description: "Emeritus Professor Chem Nayar, CEO Regen Power, is the finalist for the 2021 WA Australian of the Year.",
+        badge: null,
+      },
+      {
+        title: "A High Growth Company",
+        description: "Australian Financial Review Fast 100 Companies List — Financial Times High-Growth Companies in Asia-Pacific 2020. Nikhil Jayaraj, Managing Director, was named winner of Business News 40under40 2020.",
+        badge: null,
+      },
+      {
+        title: "Rising Stars People's Choice Awards",
+        description: "Winner of the 2019 Rising Stars People's Choice in Western Australia. Certified Tesla Installer — Completed 25,000 installations",
+        badge: null,
+      },
+      {
+        title: "20,000 Installations",
+        description: "Completed 20,000 residential, commercial and off-grid solar installations throughout Australia. Our 15th Year Anniversary as one of the top consistent solar panels installers Perth",
+        badge: null,
+      },
+      {
+        title: "15,000 Installations",
+        description: "Regen completed 15,000 residential, commercial and off-grid solar installations throughout Australia.",
+        badge: null,
+      },
+      {
+        title: "Western Australian Knowledge",
+        description: "Born and bred in Perth, WA, in 2003 when the market was still learning about rooftop solar PV systems, Regen Power is the pioneer in residential and commercial solar power system and solar panel solutions. We have installed over 10,000 residential rooftop solar systems Australia-wide and led the charge into other countries to start projects with both economic and educational implication in sustainability living.",
+        badge: null,
+      },
+      {
+        title: "Spanning Continents",
+        description: "Besides focusing on taking our own HybridGEN (integrated Renwable PowerPack) System to the Australian commercial and industrial market, Regen Power is engaged in business development in developing countries in Africa, India and South East Asia. We provide residential rooftop and commercial grid connected and off-grid solar power solutions through local partners, as well as engineering design and consultation on utility scale projects ranging from 500kW to 100MW.",
+        badge: null,
+      },
+      {
+        title: "Crossed 10,000 PV installations",
+        description: "By 2015 we crossed over 10,000 solar installations, over 1000 HybridGEN-backed telecom power supplies, and several remote community projects.",
+        badge: null,
+      },
+      {
+        title: "Finalist in Australian Technology Competition",
+        description: "Regen was a finalist in the Australian Technology Competition in 2014. Professor Chem Nayar was selected as the First Fellow of the Australian Solar Council.",
+        badge: null,
+      },
+      {
+        title: "Crossed 8,000 PV installations",
+        description: "By 2014 we crossed over 8,000 solar installations.",
+        badge: null,
+      },
+      {
+        title: "Crossed 6,000 PV installations",
+        description: "Crossed 6,000 PV installations.",
+        badge: null,
+      },
+      {
+        title: "Sustainable Energy Industry's Ambassador Award",
+        description: "Regen Power has invested heavily in R&D to invent sustainable technologies that would save fuel, electricity cost and reduce pollution. The patented technology is deployed in major mining sites in Western Australia. As a testimony for our commitment to sustainable energy development, Regen Power was conferred the prestigious Sustainable Energy Industry’s Ambassador Award and the Product Technology Award in 2011.",
+        badge: null,
+      },
+      {
+        title: "Patented HybridGEN",
+        description: "Regen has pioneered an innovative solar integrated power system, named HybridGEN™. This system is based on Professor Nayar’s patented variable speed diesel generator technology (US 7072194 & PCT/AU2011/001068).",
+        badge: null,
+      },
+      {
+        title: "SHCP Scheme",
+        description: "Installed over 1000 residential systems under Solar Homes & Communities Plan (SHCP)",
+        badge: null,
+      },
+      {
+        title: "48kW Eco-Beach Resort, Broome.",
+        description: "Delivered a turn-key solar PV/Diesel hybrid power system for the Eco Beach Wilderness Resort in Broome, A$1.1 Million",
+        badge: null,
+      },
+      {
+        title: "Wind / PV / Diesel Micro Grid System - Maldives",
+        description: "A renewable energy power plan was proposed to implement through a program of deployment of solar and wind systems in stand-alone systems and with mini-grid distribution village power for the islands in the Republic of Maldives",
+        badge: null,
+      },
+      {
+        title: "ARC Linkage Project",
+        description: "Grant amount: $270,000 Innovative Grid-Connected, Small-Scale Wind Turbine Generators Offering Low Cost and Wide Operating Speed Range",
+        badge: null,
+      },
+      {
+        title: "Asian Development Bank Project",
+        description: "Grant amount: $150,000 Rural Electrification Institutional Strengthening Project",
+        badge: null,
+      },
+      {
+        title: "Our Beginning",
+        description: "In 2003, Regen Power was established in Perth and is renowned for both innovation in the field of renewable energy and the service of our dedicated team. The company has secured its market share in Western Australia under the leadership of founding Managing Director, Professor Chem Nayar, who has over 30 years’ experience in applied research and commercialisation of alternative energy sources. In 1988, Professor Nayar established the Centre for Renewable Energy and Sustainable Technologies Australia at Curtin University and successfully managed the Centre for 15 years.",
         badge: null,
       },
     ],
